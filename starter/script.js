6eb6b0a5196e79c3c0380ae7aa16c47f9161849b
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2020-04-11 11:50:12 +0100

    firt commit!

---
+++ starter/script.js
@@ -0,0 +1,70 @@
+
+
+/*
+
+var john = {
+    fullName: 'John Smith',
+    mass: 110,
+    height: 1.95,
+    calcBMI: function() {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+}
+
+var mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function() {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+}
+
+if (john.calcBMI() > mark.calcBMI()) {
+    console.log(john.fullName + ' has a higher BMI of ' + john.bmi);
+} else if (mark.bmi > john.bmi) {
+    console.log(mark.fullName + ' has a higher BMI of ' + mark.bmi);
+} else {
+    console.log('They have the same BMI');
+}
+
+var lastName = prompt('what is your name?', 'Harry Potter');
+alert(lastName);
+*/
+
+var age = [14,16,18,20,24]
+for (var i = 0; i <= age.length; index++) {
+};
+
+var gate = function(age){
+    if (age >= 18 ){
+        console.log('you are older than ' + 18 + '. you drink beer!');
+    }else{
+        console.log('you can have a juice');
+    }
+
+}
+gate(age[i]);
+
+
+
+
+
+
+
+/*
+age[0] >=18 ? console.log('drinks a beer')
+: console.log('drinks a juice');
+*/
+
+
+
+
+
+
+
+
+
+
