509a4668fb9586ac9d6611cf97feb85b8a2e238d
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2020-04-12 19:50:09 +0100

    challenge 5, part 1

--- starter/script.js
+++ starter/script.js
@@ -59,6 +59,78 @@ age[0] >=18 ? console.log('drinks a beer')
  */
 
 // lets see what this bullshit does
+/*
+// for loop
+var john = ['John', 'Smith', 1980, 'teacher', false];
+for (var i = 0; i < john.length; i++){// i++ to i = i + 1,   i += 2 to i = i +2...
+    console.log(john[i]);
+}
+
+// while loop
+var john = ['John', 'Smith', 1980, 'teacher', false];
+var i = 0;
+while(i < john.length){
+    console.log(john[i]);
+    i++
+}
+// continue and break statements
+
+*/
+
+/*
+
+var john = ['John', 'Smith', 1980, 'teacher', false];
+for (var i = 0; i < john.length; i++){// i++ to i = i + 1,   i += 2 to i = i +2...
+    if(typeof john[i] !== 'string') continue;
+    console.log(john[i]);
+  
+}
+
+var john = ['John', 'Smith', 1980, 'teacher', false];
+for (var i = 0; i < john.length; i++){// i++ to i = i + 1,   i += 2 to i = i +2...
+    if(typeof john[i] !== 'string') break;
+    console.log(john[i]);
+  
+}
+
+*/
+// challenge 5, part 1
+
+var john = {
+
+    fullName: 'John Smith',
+    bills: [124, 48, 268, 180, 42 ],
+    tip: function(){
+
+        this.tips = [];
+        this.finalValues = [];
+
+        for(var i = 0; i < this.bills.length; i++){
+
+            var percentage;
+
+            var bill = this.bills[i];
+
+            if(bill < 50){
+                percentage = 0.2;
+    
+            } else if (bill >= 50 && bill < 200){
+                percentage = 0.15;
+    
+            } else {
+                percentage = 0.1;
+            }
+            this.tips[i] = percentage * bill;
+            this.finalValues[i] = percentage * bill + bill;
+
+        }
+    }
+
+
+}
+
+john.tip();
+console.log(john);
 
 
 
